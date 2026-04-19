3346c45a0d99ae8afd8b6a86db0c02c4120c5fc9
Meshkat77 <meshkat.oytizzo@gmail.com>
2026-04-19 14:15:45 +0600

    Task 2 NestJS Course Management API

---
+++ Task 2/task2/src/course/course.controller.ts
@@ -0,0 +1,88 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+import { CourseService } from './course.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Controller('course')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+  getAll() {
+    return this.courseService.getAllCourses();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.courseService.getCourseById(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateCourseDto) {
+    return this.courseService.createCourse(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateCourseDto) {
+    return this.courseService.updateCourse(id, dto);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') id: string, @Body() dto: UpdateCourseDto) {
+    return this.courseService.patchCourse(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.courseService.deleteCourse(id);
+  }
+
+  // ✅ FILE UPLOAD ROUTE
+  @Post(':id/upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const unique = Date.now() + '-' + file.originalname;
+          cb(null, unique);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        const allowed = /jpg|jpeg|png|pdf/;
+        const ext = extname(file.originalname).toLowerCase();
+
+        if (allowed.test(ext)) {
+          cb(null, true);
+        } else {
+          cb(new Error('Only jpg, png, pdf allowed'), false);
+        }
+      },
+      limits: {
+        fileSize: 2 * 1024 * 1024,
+      },
+    }),
+  )
+  uploadFile(
+    @Param('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.courseService.uploadCourseMaterial(id, file);
+  }
+}
